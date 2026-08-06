430cb8ffcd9d22ed33bdb03692cc7a4209df43e1
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-06 18:56:36 +0530

    chore: configure prisma with aiven postgresql

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 const envSchema = z.object({
 
     PORT:z.coerce.number().default(3004),
-    NODE_ENV:z.enum( ["development", "staging", "production"] )
+    NODE_ENV:z.enum( ["development", "staging", "production"] ),
+    DATABASE_URL:z.url()
 });
 
 const env = envSchema.parse(process.env);
