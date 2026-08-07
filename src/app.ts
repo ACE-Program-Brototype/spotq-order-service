714483daa47922bbb3d5062c386a694429ac9169
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-07 12:23:21 +0530

    chore: configure swagger api documentation

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import compression from "compression";
 import cors from "cors";
 import STATUS from "./common/constants/http-status.js";
 import httpLogger from "./infrastructure/logger/pino-http.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger.js";
 
 const app:Express = express();
 
@@ -17,6 +19,20 @@ app.use(compression());
 app.use(express.json());
 
 app.use(httpLogger);
+
+app.use("/api/docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
+
+/**
+ * @openapi
+ * /health:
+ *   get:
+ *     summary: Health check endpoint
+ *     tags:
+ *       - Health
+ *     responses:
+ *       200:
+ *         description: Service is healthy
+ */
 
 app.get(ROUTES.HEALTH , (req,res) => {
 
