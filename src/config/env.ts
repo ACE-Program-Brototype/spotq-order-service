812ef7f5578ece26adf435c82e0641b540064972
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-07 11:39:21 +0530

    chore: configure redis cloud connection for bullmq

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,9 +2,10 @@ import { z } from "zod";
 
 const envSchema = z.object({
 
-    PORT:z.coerce.number().default(3004),
+    PORT:z.coerce.number().default(3002),
     NODE_ENV:z.enum( ["development", "staging", "production"] ),
-    DATABASE_URL:z.url()
+    DATABASE_URL:z.url(),
+    REDIS_URL:z.url()
 });
 
 const env = envSchema.parse(process.env);
---
+++ src/infrastructure/redis/redis.ts
@@ -0,0 +1,10 @@
+import { Redis } from "ioredis";
+import env from "../../config/env.js";
+
+const redisConnection = new Redis(env.REDIS_URL,{
+
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false
+});
+
+export default redisConnection;
