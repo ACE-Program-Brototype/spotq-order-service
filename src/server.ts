c06600c28f680fcf9c892d1b41dce67f852b5a5c
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-07 17:14:48 +0530

    refactor: reorganize application startup flow

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import app from "./app.js";
 import env from "./config/env.js";
+import prisma from "./infrastructure/database/prisma.js";
 import logger from "./infrastructure/logger/pino.js";
+import redisConnection from "./infrastructure/redis/redis.js";
 
-app.listen(env.PORT, () => {
-  logger.info(`Server running on ${env.PORT}`);
-});
+async function startServer() {
+    try {
+
+        await prisma.$connect();
+        logger.info("Connected to PostgreSQL");
+
+        await redisConnection.ping();
+        logger.info("Connected to Redis");
+
+        app.listen(env.PORT, () => {
+            logger.info(`Server running on port ${env.PORT}`);
+        });
+
+    } catch (error) {
+      
+        logger.fatal(error, "Failed to start Order Service");
+        process.exit(1);
+    }
+}
+
+startServer();
