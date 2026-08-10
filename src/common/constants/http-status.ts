e2dd7dd00b39faf22fc8273d4023146bf68feee1
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-10 16:32:34 +0530

    refactor: replace status constants with enums

--- src/common/constants/http-status.ts
+++ src/common/constants/http-status.ts
@@ -1,15 +1,19 @@
+enum SuccessStatus {
+	OK = 200,
+	CREATED = 201,
+}
+
+enum ErrorStatus {
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	FORBIDDEN = 403,
+	NOT_FOUND = 404,
+	CONFLICT = 409,
+}
+
 const STATUS = {
-	SUCCESS: {
-		OK: 200,
-		CREATED: 201,
-	},
-	ERROR: {
-		CONFLICT: 409,
-		BAD_REQUEST: 400,
-		NOT_FOUND: 404,
-		UNAUTHORIZED: 401,
-		FORBIDDEN: 403,
-	},
-} as const;
+	SUCCESS: SuccessStatus,
+	ERROR: ErrorStatus,
+};
 
 export default STATUS;
