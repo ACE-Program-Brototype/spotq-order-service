6628c88ccee515831b1c13f42c254c6c0d9815f6
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-11 13:39:41 +0530

    chore: added swagger doc

--- src/interfaces/routes/metrics.route.ts
+++ src/interfaces/routes/metrics.route.ts
@@ -3,6 +3,25 @@ import { Router } from "express";
 
 const metricsRouter: Router = Router();
 
+/**
+ * @swagger
+ * /metrics:
+ *   get:
+ *     summary: Get Prometheus metrics
+ *     description: Returns application metrics in Prometheus exposition format.
+ *     tags:
+ *       - Metrics
+ *     responses:
+ *       200:
+ *         description: Prometheus metrics
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *       500:
+ *         description: Failed to retrieve metrics
+ */
+
 metricsRouter.get("/", async (_req, res) => {
 	res.set("Content-Type", prometheusClient.register.contentType);
 	res.end(await prometheusClient.register.metrics());
